18adbd9aa41c2017c1a83864996823e1da0e2be0
Black Shadow <35755230+rbaro022@users.noreply.github.com>
2020-10-24 22:25:03 +0530

    my food website

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// select element function
+
+const selectElement = function(element){
+	return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function() {
+	body.classList.toggle('open');
+});
+
+// Scrolling sticky
+
+window.addEventListener('scroll', () => {
+	const headerSticky = document.querySelector('header');
+
+	headerSticky.classList.toggle('sticky', window.scrollY > 50);
+});
+
+// Scroll reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+	origin: 'left',
+	duration: 1000,
+	distance: '25rem',
+	delay: 300
+});
+
+sr.reveal('.animate-right', {
+	origin: 'right',
+	duration: 1000,
+	distance: '25rem',
+	delay: 600
+});
+
+sr.reveal('.animate-top', {
+	origin: 'top',
+	duration: 1000,
+	distance: '25rem',
+	delay: 600
+});
+
+sr.reveal('.animate-bottom', {
+	origin: 'bottom',
+	duration: 1000,
+	distance: '25rem',
+	delay: 600
+});
